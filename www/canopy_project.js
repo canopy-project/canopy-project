7d14672eb95cca90a3f0de88cfadc60c89ee89a0
Greg Prisament <gregjp@toruslabs.com>
2014-07-28 09:40:35 -0700

    New homepage cleanup and development

---
+++ www/canopy_project.js
@@ -0,0 +1,18 @@
+function RenderTopbar(page) {
+    document.write("<div style='min-width:1004px'>\
+    <div class='topbar-outer'>\
+        <div class=center_channel>\
+            <span class=logo-topbar>Canopy</span>\
+            <div style='float:right'>\
+                <div class='topbar_button w400'>Home</div>\
+                <div class=topbar_button><a href=developer_zone.html>Developer Zone</a></div>\
+                <div class=topbar_button>Support Us</div>\
+                <div style='margin-left:30px' class='button_link'>LOGIN</div>\
+            </div>\
+        </div>\
+    </div>\
+</div>");
+}
+
+$(document).ready(function() {
+});
