4820ba92cd046b514080b1683477669283ba0890
Greg Prisament <greg@canopy.link>
2014-11-21 09:58:32 -0800

    Get topbar working

--- www/canopy_project.js
+++ www/canopy_project.js
@@ -2,25 +2,25 @@ function RenderTopbar(page) {
     function writeLink(link, name, selected) {
         var _RELATIVE_PATH = (typeof RELATIVE_PATH == "undefined") ? "" : RELATIVE_PATH;
         if (selected) {
-            return "<div class='topbar_button w400'><a href=" + _RELATIVE_PATH + link + ">" + name + "</a></div>";
+            return "<div class='topbar-button w400'><a href=" + _RELATIVE_PATH + link + ">" + name + "</a></div>";
         }
         else {
-            return "<div class='topbar_button'><a href=" + _RELATIVE_PATH + link + ">" + name + "</a></div>";
+            return "<div class='topbar-button'><a href=" + _RELATIVE_PATH + link + ">" + name + "</a></div>";
         }
     }
-    document.write("<div style='min-width:1004px'>\
-    <div class='topbar-outer'>\
-        <div class=center_channel>\
-            <span class=logo-topbar>Canopy</span>\
-            <div style='float:right'>"
-                + writeLink("index.html", "Home", (page == "home"))
-                + writeLink("developer_zone.html", "Developer Zone", (page == "devzone"))
-                + writeLink("support_us.html", "Support Us", (page == "supportus")) + "\
-                <a href=http://app.canopy.link><div class='button_link'>LOGIN</div></a>\
+    document.write(
+        "<div class='topbar'>\
+            <div class='topbar-inner'>\
+                <div class='topbar-middle-part'>"
+                    + writeLink("index.html", "Home", (page == "home"))
+                    + writeLink("devzone/index.html", "Developer Zone", (page == "devzone"))
+                    + writeLink("support_us.html", "Services", (page == "supportus")) + "\
+                </div>\
+                <div class='topbar-right-part'>\
+                    <div class='topbar-login'>LOGIN</div>\
+                </div>\
             </div>\
-        </div>\
-    </div>\
-</div>");
+        </div>");
 }
 
 function RenderDevZoneMenu(page) {
